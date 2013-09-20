c9da09fe54f46ef1d6f9a002ae3c947e9c832680
kellimohr <kellimohr@gmail.com>
2013-09-20 12:23:00 -0700

    Created initial app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gratitude::Application.config.secret_token = '01b35082e7dd1d479cea080320952b0acf7ac8c15dddc7421a1dfb811c2a09a9c5ab8a1e97b9bec42b17bb671a05e43fd0ad8963d2759c8daf326303bbf3be12'
